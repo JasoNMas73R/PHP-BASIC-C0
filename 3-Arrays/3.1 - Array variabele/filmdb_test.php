571a3908a12c0cba8def1f581dffdb71b7cc4f42
Jason Boon <jason.boon@student.rocva.nl>
2022-04-04 21:27:38 +0200

    opdr3.1 af

---
+++ 3-Arrays/3.1 - Array variabele/filmdb_test.php
@@ -0,0 +1,12 @@
+<?php
+
+$myMovies = array("Spiderman", "Batman", "The Joker");
+
+//vanaf php versie 5.6 mag je ook dit doen:
+$myMovies = ["Spiderman", "Batman", "The Joker"];
+
+//FYI: jij gebruikt waarschijnlijk versie 7.X
+
+echo $myMovies[1]; //als het datatype achter deze index 0 een string, of een getal is dan wordt het resultaat op het scherm getoond.
+
+?>
--- 3-Arrays/3.1 - Array variabele/index.php
+++ 3-Arrays/3.1 - Array variabele/index.php
@@ -4,6 +4,14 @@
 
 <?php
 
+
+$myMovies = array("Spiderman", "Batman", "The Joker", "Superman Returns");
+
+echo "<ul><li>$myMovies[0]</li>";
+echo "<li>$myMovies[1]</li>";
+echo "<li>$myMovies[2]</li></ul>";
+
+
 ?>
 
 </body>
