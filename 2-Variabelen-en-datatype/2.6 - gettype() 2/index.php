828da629be9acf70b21915d4c7b3696c2a3f5694
Jason Boon <jason.boon@student.rocva.nl>
2022-04-04 17:44:12 +0200

    opdr2.6 af

--- 2-Variabelen-en-datatype/2.6 - gettype() 2/index.php
+++ 2-Variabelen-en-datatype/2.6 - gettype() 2/index.php
@@ -4,6 +4,20 @@
 
 <?php
 
+
+$cheese = "Never gonna see this";
+$el = 69;
+$hovercraft = 4.20;
+$google = false;
+$zero = NULL;
+
+echo gettype($cheese);
+echo gettype($el);
+echo gettype($hovercraft);
+echo gettype($google);
+echo gettype($zero);
+
+
 ?>
 
 </body>
