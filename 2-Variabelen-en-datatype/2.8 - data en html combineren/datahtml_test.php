7695ce2dd3a8e592038b666bb47ec5901a528c28
Jason Boon <jason.boon@student.rocva.nl>
2022-04-04 17:58:37 +0200

    opdr2.8 af

--- 2-Variabelen-en-datatype/2.8 - data en html combineren/datahtml_test.php
+++ 2-Variabelen-en-datatype/2.8 - data en html combineren/datahtml_test.php
@@ -1,18 +1,11 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
 
 
 $voornaam1 = "Jason";
 $voornaam2 = "Jamiro";
-$voornaam3 = "Jaren";
+$voornaam3 = "Joshua";
 
 echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>";
 
 
 ?>
-
-</body>
-</html> 
