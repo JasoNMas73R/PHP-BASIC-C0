f141459a74a05b5f8a5867b64ce9dfc9327ac9af
Jason Boon <jason.boon@student.rocva.nl>
2022-04-04 17:51:13 +0200

    opdr2.7 af

--- 2-Variabelen-en-datatype/2.7 - data en tekst combineren/index.php
+++ 2-Variabelen-en-datatype/2.7 - data en tekst combineren/index.php
@@ -4,6 +4,14 @@
 
 <?php
 
+
+$mijnLeeftijd = 19;
+$voornaam = "Jason";
+
+echo "Jouw naam is $voornaam en je bent $mijnLeeftijd jaar oud. <br>";
+echo "Waar hierboven $voornaam en $mijnLeeftijd staat moet natuurlijk jouw echte leeftijd en naam staan!"
+
+
 ?>
 
 </body>
