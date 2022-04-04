18b477d9eae2d85b5b9669227294223cea971e7e
Jason Boon <jason.boon@student.rocva.nl>
2022-04-04 18:11:42 +0200

    opdr2.9 af

--- 2-Variabelen-en-datatype/2.9 - String concatenation/index.php
+++ 2-Variabelen-en-datatype/2.9 - String concatenation/index.php
@@ -4,6 +4,14 @@
 
 <?php
 
+
+$mijnLeeftijd = 19;
+$voornaam = "Jason";
+
+echo "Jouw naam is " . $voornaam . " en je bent " . $mijnLeeftijd . " jaar oud. <br>";
+echo 'Waar hierboven $voornaam en $mijnLeeftijd staan moet natuurlijk jouw echte naam en leeftijd staan!';
+
+
 ?>
 
 </body>
