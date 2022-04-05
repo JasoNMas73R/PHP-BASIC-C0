1bf1a6bac36ef8fd6e5dd036e0cf3970a926cc12
Jason Boon <jason.boon@student.rocva.nl>
2022-04-05 21:05:36 +0200

    opdr3.2 af

--- 3-Arrays/3.2 - Array waarde wijzigen/index.php
+++ 3-Arrays/3.2 - Array waarde wijzigen/index.php
@@ -4,6 +4,23 @@
 
 <?php
 
+
+$myDogNames = array('Fido Castro', 'Ludovic', 'Sebastian', 'Odie');
+
+echo "$myDogNames[0], ";
+echo "$myDogNames[1], ";
+echo "$myDogNames[2], ";
+echo "$myDogNames[3]. ";
+
+$myDogNames[3] = 'Albert';
+// $myDogNames[3] = $myDogNames[2];
+
+echo "<br><br> $myDogNames[3], ";
+echo "$myDogNames[0], ";
+echo "$myDogNames[1], ";
+echo "$myDogNames[2]. ";
+
+
 ?>
 
 </body>
