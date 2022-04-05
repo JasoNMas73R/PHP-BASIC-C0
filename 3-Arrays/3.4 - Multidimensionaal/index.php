12943a40b935b52890a59edfa3fb74f34feaab24
Jason Boon <jason.boon@student.rocva.nl>
2022-04-05 21:47:40 +0200

    opdr3.4 af

--- 3-Arrays/3.4 - Multidimensionaal/index.php
+++ 3-Arrays/3.4 - Multidimensionaal/index.php
@@ -4,6 +4,26 @@
 
 <?php
 
+
+$abcGetallen = array(   //hulp van https://www.youtube.com/watch?v=G1iDSoAXyvM
+    'A', 
+    'B', 
+    'C', 
+    'E', 
+    'F',
+    [24, 56, 72, 90]
+);
+
+print_r($abcGetallen);
+
+echo "<br><br>";
+echo $abcGetallen[0];
+echo $abcGetallen[5][1];
+echo ", ";
+echo $abcGetallen[2];
+echo $abcGetallen[5][3]; //tjonge jonge wat een gedoe
+
+
 ?>
 
 </body>
---
+++ 3-Arrays/3.4 - Multidimensionaal/list_test.php
@@ -0,0 +1,12 @@
+<?php
+
+// $lijstDerDingen = [true,"true", 1, 0, "false",false,5.5];
+
+$complexeLijst = [ [1,2,3], ["een", "twee", "drie"], true, false  ];
+
+$complexeLijst = [ [1,2,3], ["een", "twee", "drie"], true, false  ];
+
+echo $complexeLijst[0][1];
+
+
+?>
