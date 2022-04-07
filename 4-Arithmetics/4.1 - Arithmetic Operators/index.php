ea2bf85a470579ca8a7165dc5ba25e4ae6aa658f
Jason Boon <jason.boon@student.rocva.nl>
2022-04-07 17:59:45 +0200

    opdr4.1 af

--- 4-Arithmetics/4.1 - Arithmetic Operators/index.php
+++ 4-Arithmetics/4.1 - Arithmetic Operators/index.php
@@ -1,10 +1,29 @@
 <!DOCTYPE html>
 <html>
 <body>
+    <title>index.php?=PHPE9568F36-D428-11d2-A769-00AA001ACF42</title>
 
 <?php
+
+
+$hahaGetal = 69;
+$wietGetal = 420;
+
+echo $hahaGetal." plus "; //geen idee waarom het niet in een regel kan, anders gaat hij zeiken
+echo $wietGetal." is: ".$hahaGetal + $wietGetal;
+echo ". <br>";
+echo $hahaGetal." minus ";
+echo $wietGetal." is: ".$hahaGetal - $wietGetal;
+echo ". <br>";
+echo $hahaGetal." keer ";
+echo $wietGetal." is: ".$hahaGetal * $wietGetal;
+echo ". <br>";
+echo $hahaGetal." gedeeld door ";
+echo $wietGetal." is: ".$hahaGetal / $wietGetal;
+echo ". <br>";
+
 
 ?>
 
 </body>
-</html> 
+</html>
