edcb59e01d7daf7f4dad5b56988aaf0f67163b36
Jason Boon <jason.boon@student.rocva.nl>
2022-04-07 18:20:01 +0200

    opdr4.2 af

--- 4-Arithmetics/4.2 -Assignment Operators/index.php
+++ 4-Arithmetics/4.2 -Assignment Operators/index.php
@@ -1,8 +1,24 @@
 <!DOCTYPE html>
 <html>
 <body>
+    <title>index.php</title>
 
 <?php
+
+
+$nietzograppigGetal = 666;
+
+echo "<b>".$nietzograppigGetal."</b> is het getal. <br><br>";
+echo $nietzograppigGetal." plus 10 is: <b>".$nietzograppigGetal + 10;
+echo "</b>. <br>";
+echo $nietzograppigGetal." minus 10 is: <b>".$nietzograppigGetal - 10; // geeft hier een verkeerd antwoord als ik het in dezelfde regel doe op jullie manier
+echo "</b>. <br>";
+echo $nietzograppigGetal." keer 10 is: <b>".$nietzograppigGetal * 10;
+echo "</b>. <br>";
+echo $nietzograppigGetal." gedeeld door 10 is: <b>".$nietzograppigGetal / 10; // geeft hier een verkeerd antwoord als ik het in dezelfde regel doe op jullie manier
+echo "</b>.";
+
+
 
 ?>
 
