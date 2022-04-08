fcad388c0482e887efe028e0013d40a6c923496d
Jason Boon <jason.boon@student.rocva.nl>
2022-04-08 17:20:05 +0200

    opdr5.1 ag

--- 5-Conditional_Statements/5.1 - Comparison Operators/index.php
+++ 5-Conditional_Statements/5.1 - Comparison Operators/index.php
@@ -1,8 +1,30 @@
 <!DOCTYPE html>
 <html>
 <body>
+    <title>index.php</title>
 
 <?php
+
+
+$test1 = 121;
+$test2 = "Happy day!";
+
+var_dump($test1 == $test2);  // links is gelijk aan rechts
+echo "<br>";
+var_dump($test1 === $test2); // links is identiek aan rechts (zelfde waarde en zelfde datatype)
+echo "<br>";
+var_dump($test1 != $test2);  // links is ongelijk aan rechts
+echo "<br>";
+var_dump($test1 > $test2);   // links is groter dan rechts
+echo "<br>";
+var_dump($test1 < $test2);   // links is kleiner dan rechts
+echo "<br>";
+var_dump($test1 <> $test2);  // links is ongelijk aan rechts
+echo "<br>";
+var_dump($test1 >= $test2);  // links is groter of gelijk aan rechts
+echo "<br>";
+var_dump($test1 <= $test2);  // links is kleiner of gelijk aan rechts
+
 
 ?>
 
