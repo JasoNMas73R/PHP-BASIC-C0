5261c2a10e5f346f1e4ef396b66d32abd405f9fb
Jason Boon <jason.boon@student.rocva.nl>
2022-04-08 17:07:47 +0200

    opdr4.3 af

--- 4-Arithmetics/4.3 - Increment-operators/index.php
+++ 4-Arithmetics/4.3 - Increment-operators/index.php
@@ -1,8 +1,20 @@
 <!DOCTYPE html>
 <html>
 <body>
+    <title>index.php</title>
 
 <?php
+
+
+$getal01 = 58;
+
+echo "Ons getal van vandaag is ".$getal01;
+echo ". <br>";
+echo "Je hebt één punt gescoord. Goed zo! Jouw score is nu ".++$getal01;
+echo ". <br><br>";
+echo "Helaas... Je hebt één punt verloren. Jouw score is nu ".--$getal01;
+echo ".";
+
 
 ?>
 
