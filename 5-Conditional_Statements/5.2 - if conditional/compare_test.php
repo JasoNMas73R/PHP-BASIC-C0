13f8eb39a918b93c8e45b95040c75a786c3e7f88
Jason Boon <jason.boon@student.rocva.nl>
2022-04-08 17:34:34 +0200

    opdr5.2 af

---
+++ 5-Conditional_Statements/5.2 - if conditional/compare_test.php
@@ -0,0 +1,19 @@
+<title>compare_test.php</title>
+
+<?php
+
+
+$variabele1 = 10;
+$variabele2 = 10; // 12
+
+if($variabele1 == $variabele2) {
+    echo "De vergelijking is waar!";
+}
+
+else {
+    echo "De vergelijking is niet waar!";
+}
+
+// nog herrinderd van python
+
+?>
--- 5-Conditional_Statements/5.2 - if conditional/index.php
+++ 5-Conditional_Statements/5.2 - if conditional/index.php
@@ -1,8 +1,42 @@
 <!DOCTYPE html>
 <html>
 <body>
+    <title>index.php</title>
 
 <?php
+
+
+$test1 = 12;
+$test2 = 3;
+
+if($test1 == $test2) {
+    echo "gelijk";
+}
+echo "<br>";
+if($test1 != $test2) {
+    echo "ongelijk";
+}
+echo "<br>";
+if($test1 === $test2) {
+    echo "identiek";
+}
+echo "<br>";
+if($test1 > $test2) {
+    echo "test1 is groter dan test2";
+}
+echo "<br>";
+if($test1 < $test2) {
+    echo "test1 is kleiner dan test2";
+}
+echo "<br>";
+if($test1 >= $test2) {
+    echo "test1 is groter of gelijk aan test2";
+}
+echo "<br>";
+if($test1 <= $test2) {
+    echo "test1 is kleiner of gelijk aan test2";
+}
+
 
 ?>
 
