cf95ff46be1c87f109355195293de965c47747cd
Jason Boon <jason.boon@student.rocva.nl>
2022-04-11 10:53:43 +0200

    opdr6.1 af

--- 6-Functions/6.1 - function declaration/index.php
+++ 6-Functions/6.1 - function declaration/index.php
@@ -1,8 +1,23 @@
 <!DOCTYPE html>
 <html>
 <body>
+    <title>index.php</title>
 
 <?php
+
+
+function zetWoordenAanElkaar() { // gewoon hier een uur naar lopen te kijken gisten
+                                 // zonder dat me opviel dat ik gewoon function moet
+    $woordje = "Hallo";          // zetten voor de naam, godverdomme man klote zooi
+    $zinnetje = "mooie blauwe planeet!";
+    $heleZin = "$woordje $zinnetje";
+
+    echo $heleZin;
+
+}
+
+zetWoordenAanElkaar()
+
 
 ?>
 
