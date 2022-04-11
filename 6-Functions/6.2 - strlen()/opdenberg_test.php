93b61e3a40170df788e88fe832d787098691f759
Jason Boon <jason.boon@student.rocva.nl>
2022-04-11 14:29:10 +0200

    opdr6.2 af

--- 6-Functions/6.2 - strlen()/opdenberg_test.php
+++ 6-Functions/6.2 - strlen()/opdenberg_test.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
     <body>
@@ -8,13 +9,15 @@
 
 $voornaam = "Jason";
 $achternaam = "Boon";
-$geheleNaam = "Mijn naam is $voornaam $achternaam.";
+$geheleNaam = "$voornaam $achternaam";
 
 function mijnNaamGROOT($geheel) { // een nieuwe variabel tussen haakjes zetten houdt in dat als je de functie oproept met een andere variabel tussen de haakjes dat de gebruikte variabel bij de call gelijk staat aan de nieuwe variabel geïntroduceerd in de functie.
     echo strtoupper($geheel);
 }
 
+echo "Mijn naam is ";
 mijnNaamGROOT($geheleNaam); // $geheleNaamGROOT staat in deze call alleen gelijk aan $gehaal in de functie
+echo ".";
 
 
 ?>
